0a3b579b77d86cf68e084978bad817a7ff256bfd
Dmitriy B <kupperfeet@gmail.com>
2022-11-04 18:31:41 +0300

    task 1 done

--- task_1.py
+++ task_1.py
@@ -1,2 +1,2 @@
 # напечатать сторку в одну линию - C:\WINDOWS\System32\drivers\etc\nst
-print(F'Ваша строка: ', 'C:\\WINDOWS\\System32\\drivers\\etc\\nst')
+print(r'Ваша строка: C:\\WINDOWS\\System32\\drivers\\etc\\nst')
